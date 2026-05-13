f915a3200a261714c6dc062f766b266c8d6131bc
Kiaro Mendoza <mendozakiaro@gmail.com>
2026-05-13 15:18:52 +0800

    describe mo kung ano binago mo

--- test_mail.php
+++ test_mail.php
@@ -1,4 +1,25 @@
 <?php
 require 'mailer.php';
-$result = sendMail('mendozakiaro@gmail.com', 'Test', 'Test Email', '<p>Test lang</p>');
-echo $result ? 'EMAIL SENT!' : 'FAILED: ' . error_get_last()['message'];
+$result = sendMail('mendozalesterjames7@gmail.com', 'Test', 'Test Email', '<p>Test lang</p>');
+echo $result ? 'EMAIL SENT!' : 'FAILED: ' . error_get_last()['message'] ?? 'unknown error';
+
+// Show PHPMailer error directly
+use PHPMailer\PHPMailer\PHPMailer;
+$mail = new PHPMailer(true);
+try {
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'mendozalesterjames7@gmail.com';
+    $mail->Password   = 'kwrussfymluo wcfe';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port       = 587;
+    $mail->setFrom('mendozalesterjames7@gmail.com', 'Test');
+    $mail->addAddress('mendozalesterjames7@gmail.com');
+    $mail->Subject = 'Test';
+    $mail->Body    = 'Test';
+    $mail->send();
+    echo '<br>DIRECT TEST: SENT!';
+} catch (Exception $e) {
+    echo '<br>DIRECT ERROR: ' . $mail->ErrorInfo;
+}
