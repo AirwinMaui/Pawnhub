93af96e5990b94a330f25197aaf0410a4cd372b2
Kiaro Mendoza <mendozakiaro@gmail.com>
2026-05-13 14:57:59 +0800

    describe mo kung ano binago mo

--- mailer.php
+++ mailer.php
@@ -2,7 +2,7 @@
 // ── GMAIL SETTINGS — palitan ng sarili mong Gmail ─────────────
 define('MAIL_FROM',     'mendozakiaro@gmail.com');
 define('MAIL_FROM_NAME','PawnHub System');
-define('MAIL_USERNAME', 'aa1bc0001@smtp-brevo.com');
+define('MAIL_FROM', 'aa1bc0001@smtp-brevo.com');
 define('MAIL_PASSWORD', 'xsmtpsib-4e68dc0abe6290ed0d7e2a776b05e433436eae6eea9b9f34c423d03845ae179a-iT2PrdhDZjWcDpNZ');
 define('APP_URL', 'https://pawnhub-bjesb8gqh5d3eqfy.southeastasia-01.azurewebsites.net');
 
